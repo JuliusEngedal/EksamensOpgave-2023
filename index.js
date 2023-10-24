51fe528c40df19efc8daa8732054a509c444c463
JuliusE <julius.engedal@gmail.com>
2023-10-25 00:43:23 +0200

    Aktiviteter mangler lidt responsivt

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import navigation from "./scripts/navigation.js";
 import Footer from "./scripts/footer.js";
 import Udtalelser from "./scripts/kundeudtalelser.js";
+import activities from "./scripts/accordian.js";
 
+
+activities.init();
 Udtalelser.init();
 navigation.init();
 Footer.init();
